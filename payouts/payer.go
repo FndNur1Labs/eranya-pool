aa64cbd77227eba8f5e5a6d6bb30e3f065bed51c
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-04-22 13:02:30 +0700

    default dir

--- payouts/payer.go
+++ payouts/payer.go
@@ -10,9 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth/rpc"
-	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth/storage"
-	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth/util"
+	"github.com/FndNur1Labs/eranya-pool/rpc"
+	"github.com/FndNur1Labs/eranya-pool/storage"
+	"github.com/FndNur1Labs/eranya-pool/util"
 )
 
 const txCheckInterval = 5 * time.Second
