f77531f92e7aa86443d91c41c6c92f141815f2fa
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-04-22 11:25:40 +0700

    dir

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -7,8 +7,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/techievee/ethash-mining-pool/storage"
-	"github.com/techievee/ethash-mining-pool/util"
+	"github.com/eranya-pool/tree/V2.0_Eth_11/storage"
+	"github.com/eranya-pool/tree/V2.0_Eth_11/util"
 	"io/ioutil"
 )
 
