8bd07b7486f13faf5c320120851f5fb4ea35ab25
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-04-22 11:27:14 +0700

    dir

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/techievee/ethash-mining-pool/rpc"
-	"github.com/techievee/ethash-mining-pool/storage"
+	"github.com/eranya-pool/tree/V2.0_Eth_11/rpc"
+	"github.com/eranya-pool/tree/V2.0_Eth_11/storage"
 )
 
 func TestMain(m *testing.M) {
