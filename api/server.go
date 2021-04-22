2cec65bc01f518dd05ada8d6a1087c2d22867846
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-04-22 11:31:55 +0700

    V2.0_Eth dir

--- api/server.go
+++ api/server.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth_11/storage"
-	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth_11/util"
+	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth/storage"
+	"github.com/FndNur1Labs/eranya-pool/tree/V2.0_Eth/util"
 )
 
 type ApiConfig struct {
